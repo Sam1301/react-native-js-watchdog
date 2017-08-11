c26ef2a38d9c0954df11e2c3e7a8d7b8fd818d6f
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2017-08-11 17:21:08 +0530

    Add verbose option to RenderTest and release.

--- RenderTest.js
+++ RenderTest.js
@@ -1,6 +1,4 @@
-// originally implemented as a mixin,
-// taken from https://gist.github.com/joshma/6753333dd38a6010f9a6#file-whydidyouupdatemixin-js
-// modified to make a higher-order component
+// modified from https://gist.github.com/joshma/6753333dd38a6010f9a6#file-whydidyouupdatemixin-js
 
 /* eslint-disable no-console */
 import _ from 'underscore';
@@ -10,13 +8,13 @@ function isRequiredUpdateObject(o) {
   return Array.isArray(o) || (o && o.constructor === Object.prototype.constructor);
 }
 
-function deepDiff(o1, o2, p) {
+function deepDiff(o1, o2, p, verbose) {
   const notify = status => {
     console.warn('Update %s', status);
     console.log('%cbefore', 'font-weight: bold', o1);
     console.log('%cafter ', 'font-weight: bold', o2);
   };
-  if (!_.isEqual(o1, o2)) {
+  if (!_.isEqual(o1, o2) && verbose) {
     console.group(p);
     if ([o1, o2].every(_.isFunction)) {
       notify('avoidable?');
@@ -25,7 +23,7 @@ function deepDiff(o1, o2, p) {
     } else {
       const keys = _.union(_.keys(o1), _.keys(o2));
       for (const key of keys) {
-        deepDiff(o1[key], o2[key], key);
+        deepDiff(o1[key], o2[key], key, verbose);
       }
     }
     console.groupEnd();
@@ -35,20 +33,18 @@ function deepDiff(o1, o2, p) {
     if (_.isObject(o1) && _.isObject(o2)) {
       const keys = _.union(_.keys(o1), _.keys(o2));
       for (const key of keys) {
-        deepDiff(o1[key], o2[key], key);
+        deepDiff(o1[key], o2[key], key, verbose);
       }
     }
     console.groupEnd();
   }
 }
 
-// changes
 function isPureComponent(WrappedComponent) {
   return !!(WrappedComponent.prototype && WrappedComponent.prototype.isPureReactComponent);
 }
 
-// changes
-const RerenderTest = WrappedComponent => {
+const RerenderTest = (WrappedComponent, { verbose }) => {
   const Parent = isPureComponent(WrappedComponent) ? PureComponent : Component;
 
   return class Container extends Parent {
@@ -56,7 +52,7 @@ const RerenderTest = WrappedComponent => {
       deepDiff(
           { props: prevProps, state: prevState },
           { props: this.props, state: this.state },
-          WrappedComponent.name,
+          WrappedComponent.name, verbose
         );
     }
     render() {
